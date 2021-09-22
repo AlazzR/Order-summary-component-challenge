5048b2901cd2cf1aa889c821ce8727eb203bec7a
AlazzR <rasheedalazzoni1@gmail.com>
2021-09-22 14:17:53 -0400

    sss

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Order-summary-component-challenge">
         <Switch>
             <Route exact path="/"><App /></Route>
         </Switch>
